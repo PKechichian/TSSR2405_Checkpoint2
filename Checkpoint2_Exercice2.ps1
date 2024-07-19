9036b0f3803f72bd8a92c744ec95d82cca2840f3
PKechichian <kechichianphilippe@gmail.com>
2024-07-19 13:31:05 +0200

    Create Checkpoint2_Exercice2.ps1

---
+++ Checkpoint2_Exercice2.ps1
@@ -0,0 +1,56 @@
+Write-Host "--- Début du script ---"
+
+Function Random-Password ($length = 12)
+{
+    $punc = 46..46
+    $digits = 48..57
+    $letters = 65..90 + 97..122
+
+    $password = get-random -count $length -input ($punc + $digits + $letters) |`
+        ForEach -begin { $aa = $null } -process {$aa += [char]$_} -end {$aa}
+    Return $password
+}
+
+Function ManageAccentsAndCapitalLetters
+{
+    param ([String]$String)
+    
+    $StringWithoutAccent = $String -replace '[éèêë]', 'e' -replace '[àâä]', 'a' -replace '[îï]', 'i' -replace '[ôö]', 'o' -replace '[ùûü]', 'u'
+    $StringWithoutAccentAndCapitalLetters = $StringWithoutAccent.ToLower()
+    $StringWithoutAccentAndCapitalLetters
+}
+
+$Path = "C:\Scripts"
+$CsvFile = "$Path\Users.csv"
+$LogFile = "$Path\Log.log"
+
+$Users = Import-Csv -Path $CsvFile -Delimiter ";" -Header "prenom","nom","fonction","description" -Encoding UTF8  | Select-Object -Skip 1
+
+
+foreach ($User in $Users)
+{
+    $Prenom = ManageAccentsAndCapitalLetters -String $User.prenom
+    $Nom = ManageAccentsAndCapitalLetters -String $User.Nom
+    $Name = "$Prenom.$Nom"
+    If (-not(Get-LocalUser -Name $Name -ErrorAction SilentlyContinue))
+    {
+        $Pass = Random-Password
+        $Password = (ConvertTo-secureString $Pass -AsPlainText -Force)
+        $Description = "$($user.description) - $($User.fonction)"
+        $UserInfo = @{
+            Name                 = $Name
+            FullName             = $Name
+            Password             = $Password
+            Description          = $Description
+            AccountNeverExpires  = $true
+            PasswordNeverExpires = $true
+        }
+
+        New-LocalUser @UserInfo
+        Add-LocalGroupMember -Group "Utilisateurs" -Member $Name
+        Write-Host "L'utilisateur $Name a été crée avec le mot de passe $pass" -ForegroundColor Green
+    }
+}
+
+Read-Host -Prompt "Appuyer sur Entrée pour continuer"
+Write-Host "--- Fin du script ---"
